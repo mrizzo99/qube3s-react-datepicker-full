6af4bfbf25e08b406db8470c18aeb1f551405bb9
Mrizzo99 <mrizzo99@gmail.com>
2025-11-24 22:34:57 -0500

    Changes to attempt to make storybook work appropriately

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,9 @@ import type { StorybookConfig } from "@storybook/react";
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-essentials'],
-  framework: { name: '@storybook/react', options: {} }
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  }
 };
 export default config;
